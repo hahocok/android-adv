74926b98a82d48d5b85a5ef8c257f7a60e73e9f0
Denis <hahocok@gmail.com>
2020-11-12 10:13:56 +0300

    add lesson 1

--- app/src/main/java/com/android/android/MainActivity.java
+++ app/src/main/java/com/android/android/MainActivity.java
@@ -2,10 +2,14 @@ package com.android.android;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.annotation.Nullable;
@@ -22,12 +26,45 @@ public class MainActivity extends BaseActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        createNavMenu(toolbar);
+
         MainActivityFragment mainActivityFragment = new MainActivityFragment();
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.fragment_container, mainActivityFragment)
                 .addToBackStack("")
                 .commit();
+    }
+
+    private void createNavMenu(Toolbar toolbar) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_about_dev) {
+                    //TODO action
+                } else if (id == R.id.nav_feedback) {
+                    //TODO action
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
