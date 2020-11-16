b5c8720e193626bf747aff4f8dd5cfd8aeee2fef
Denis <hahocok@gmail.com>
2020-11-16 09:13:13 +0300

    добавил 3 урок

--- app/src/main/java/com/android/android/MainActivityFragment.java
+++ app/src/main/java/com/android/android/MainActivityFragment.java
@@ -1,8 +1,10 @@
 package com.android.android;
 
-import android.net.Uri;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -13,18 +15,8 @@ import android.widget.Toast;
 
 import com.android.android.cards.SocSource;
 import com.android.android.model.WeatherRequest;
+import com.android.android.services.ServiceReadWeather;
 import com.google.android.material.button.MaterialButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.stream.Collectors;
-
-import javax.net.ssl.HttpsURLConnection;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -34,11 +26,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivityFragment extends Fragment implements Constants {
-
-    private static final String TAG = "WEATHER";
-    private static final String WEATHER_URL_START = "https://api.openweathermap.org/data/2.5/weather?q=";
-    private static final String WEATHER_URL_END = ",RU&appid=";
-    private static final String WEATHER_API_KEY = "68c65e3c4c42de33f8a67466a1719a08";
 
     private MainPresenter presenter;
 
@@ -50,6 +37,20 @@ public class MainActivityFragment extends Fragment implements Constants {
     private TextView mainPressure;
     private TextView mainHumidity;
     private TextView mainWindSpeed;
+
+    private ServiceFinishedReceiver serviceFinishedReceiver = new ServiceFinishedReceiver();
+
+    private class ServiceFinishedReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, final Intent intent) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    displayWeather(intent);
+                }
+            });
+        }
+    }
 
     @Nullable
     @Override
@@ -63,17 +64,12 @@ public class MainActivityFragment extends Fragment implements Constants {
         mainCityContainer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SelectCityFragment selectCityFragment = new SelectCityFragment();
-
-                Bundle bundle = new Bundle();
-                bundle.putString(CITY, mainCity.getText().toString());
-                bundle.putBoolean(HUMIDITY, isVisible(mainHumidityContainer));
-
-                selectCityFragment.setArguments(bundle);
+                presenter.setCity(mainCity.getText().toString());
+                presenter.setHumidity(mainHumidityContainer);
 
                 getFragmentManager().
                         beginTransaction()
-                        .replace(R.id.fragment_container, selectCityFragment)
+                        .replace(R.id.fragment_container, new SelectCityFragment())
                         .addToBackStack("")
                         .commit();
 
@@ -115,72 +111,18 @@ public class MainActivityFragment extends Fragment implements Constants {
     }
 
     private void getWeather() {
-        final URL uri = getUrl();
-        final Handler handler = new Handler(); // Запоминаем основной поток
-        new Thread(new Runnable() {
-            public void run() {
-                HttpsURLConnection urlConnection = null;
-                try {
-                    urlConnection = (HttpsURLConnection) uri.openConnection();
-                    urlConnection.setRequestMethod("GET"); // установка метода получения данных -GET
-                    urlConnection.setReadTimeout(10000); // установка таймаута - 10 000 миллисекунд
-                    BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream())); // читаем  данные в поток
-                    String result = getLines(in);
-                    // преобразование данных запроса в модель
-                    Gson gson = new Gson();
-                    final WeatherRequest weatherRequest = gson.fromJson(result, WeatherRequest.class);
-                    // Возвращаемся к основному потоку
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            displayWeather(weatherRequest);
-                        }
-                    });
-                } catch (FileNotFoundException e) {
-                    Snackbar.make(getView(), getResources().getString(R.string.error_city_not_found), Snackbar.LENGTH_LONG).show();
-                } catch (Exception e) {
-                    Log.e(TAG, getResources().getString(R.string.error_fail_connection), e);
-                    e.printStackTrace();
-                } finally {
-                    if (null != urlConnection) {
-                        urlConnection.disconnect();
-                    }
-                }
-            }
-        }).start();
+        ServiceReadWeather.startService(getContext(), presenter.getCity());
     }
 
-    private URL getUrl() {
-        String city;
-        if (getArguments() != null) {
-            city = getArguments().getString(CITY);
-        } else {
-            city = "";
+    private void displayWeather(Intent intent){
+        final WeatherRequest weatherRequest = (WeatherRequest) intent.getSerializableExtra(INTENT_RESPONSE);
+        if (weatherRequest != null) {
+            mainCity.setText(weatherRequest.getName());
+            mainTemperature.setText(String.format("%f2", weatherRequest.getMain().getTemp()));
+            mainPressure.setText(String.format("%d", weatherRequest.getMain().getPressure()));
+            mainHumidity.setText(String.format("%d", weatherRequest.getMain().getHumidity()));
+            mainWindSpeed.setText(String.format("%f2", weatherRequest.getWind().getSpeed()));
         }
-        String sb = WEATHER_URL_START +
-                city +
-                WEATHER_URL_END;
-
-        URL uri = null;
-        try {
-            uri = new URL(sb + WEATHER_API_KEY);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        return uri;
-    }
-
-    private String getLines(BufferedReader in) {
-        return in.lines().collect(Collectors.joining("\n"));
-    }
-
-    private void displayWeather(WeatherRequest weatherRequest){
-        mainCity.setText(weatherRequest.getName());
-        mainTemperature.setText(String.format("%f2", weatherRequest.getMain().getTemp()));
-        mainPressure.setText(String.format("%d", weatherRequest.getMain().getPressure()));
-        mainHumidity.setText(String.format("%d", weatherRequest.getMain().getHumidity()));
-        mainWindSpeed.setText(String.format("%d", weatherRequest.getWind().getSpeed()));
     }
 
     private void initRecyclerView(View view, SocSource data){
@@ -205,45 +147,38 @@ public class MainActivityFragment extends Fragment implements Constants {
 
     }
 
+    private void readIntent() {
+        String city = presenter.getCity();
+        boolean isHumidity = presenter.isHumidity();
 
-    private boolean isVisible(View view) {
-        int isVisible = view.getVisibility();
-        if (isVisible == View.VISIBLE) {
-            return true;
+        if (city != null && city.equals(getResources().getString(R.string.city))) {
+            mainCity.setHint(getResources().getString(R.string.enter_city));
         } else {
-            return false;
+            mainCity.setText(city);
+            mainCity.setGravity(Gravity.CENTER_HORIZONTAL);
         }
+
+        mainTemperature.setText(presenter.getTemperature());
+
+        if (isHumidity) {
+            mainHumidityContainer.setVisibility(View.VISIBLE);
+        } else {
+            mainHumidityContainer.setVisibility(View.GONE);
+        }
+
+
     }
 
-    private void readIntent() {
-        Bundle args = getArguments();
-        String city = null;
-        boolean isCloudiness = false;
-        boolean isHumidity = false;
-        if (args != null) {
-            city = args.getString(CITY);
-            isCloudiness = args.getBoolean(CLOUDINESS, false);
-            isHumidity = args.getBoolean(HUMIDITY, false);
+    @Override
+    public void onStart() {
+        super.onStart();
+        getActivity().registerReceiver(serviceFinishedReceiver, new IntentFilter(BROADCAST_RESPONSE_WEATHER));
+    }
 
-            if (city != null && city.equals(getResources().getString(R.string.city))) {
-                mainCity.setHint(getResources().getString(R.string.enter_city));
-            } else {
-                mainCity.setText(city);
-                mainCity.setGravity(Gravity.CENTER_HORIZONTAL);
-            }
-
-            mainTemperature.setText(presenter.getTemperature());
-
-            if (isHumidity) {
-                mainHumidityContainer.setVisibility(View.VISIBLE);
-            } else {
-                mainHumidityContainer.setVisibility(View.GONE);
-            }
-        } else {
-            mainHumidityContainer.setVisibility(View.VISIBLE);
-        }
-
-
+    @Override
+    public void onStop() {
+        getActivity().unregisterReceiver(serviceFinishedReceiver);
+        super.onStop();
     }
 
     @Override
@@ -258,6 +193,6 @@ public class MainActivityFragment extends Fragment implements Constants {
         super.onSaveInstanceState(outState);
         presenter.setTemperature(mainTemperature.getText().toString());
         Toast.makeText(getContext(), "onSaveInstanceState()", Toast.LENGTH_SHORT).show();
-        Log.d(LOG_TAG, "onSaveInstanceState()");
+        Log.d(TAG, "onSaveInstanceState()");
     }
 }
